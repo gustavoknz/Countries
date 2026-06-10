c558c6ddcce7d93a3a76a385a1c35b7676f70722
Gustavo Kieling <gustavo.kieling@waitrose.co.uk>
2026-06-10 11:15:05 +0100

    Mini refactor II

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
