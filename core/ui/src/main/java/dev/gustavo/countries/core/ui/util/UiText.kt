337e5c66b7b1fcafc058fac576f272884edb6064
Gustavo Kieling <gustavoknz@gmail.com>
2026-07-01 13:28:09 +0100

    Fix unit tests

--- core/ui/src/main/java/dev/gustavo/countries/core/ui/util/UiText.kt
+++ core/ui/src/main/java/dev/gustavo/countries/core/ui/util/UiText.kt
@@ -6,7 +6,25 @@ import androidx.compose.ui.res.stringResource
 
 sealed interface UiText {
     data class DynamicString(val value: String) : UiText
-    class StringResource(@param:StringRes val resId: Int, vararg val args: Any) : UiText
+    class StringResource(@param:StringRes val resId: Int, vararg val args: Any) : UiText {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as StringResource
+
+            if (resId != other.resId) return false
+            if (!args.contentEquals(other.args)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = resId
+            result = 31 * result + args.contentHashCode()
+            return result
+        }
+    }
 
     @Composable
     fun asString(): String {
